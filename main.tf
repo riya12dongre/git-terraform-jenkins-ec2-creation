548119cccf8b080b6c1676ee2b184685a937fed7
vangimallaMahesh <105789814+vangimallaMahesh@users.noreply.github.com>
2023-04-20 17:15:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
     region = "ap-south-1"  
 }
 
-resource "aws_instance" "ubuntu" {
-  ami           = "ami-03a933af70fa97ad2 " # us-west-2
+resource "aws_instance" "ec2-user" {
+  ami           = "ami-06fc49795bc410a0c" # us-west-2
   instance_type = "t2.micro"
   tags = {
       Name = "Terraform server 2"
